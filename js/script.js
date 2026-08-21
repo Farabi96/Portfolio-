5cd3b64b6b8b5565b7473aa175807131582aa41e
Farabi96 <shafiulalfarabi@gmail.com>
2026-08-22 01:19:40 +0600

    "Add scroll to top functionality"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Portfolio loaded successfully!");
+ const scrollTopButton = document.createElement("button");
+
+scrollTopButton.innerHTML = "↑";
+scrollTopButton.className = "scroll-top";
+
+document.body.appendChild(scrollTopButton);
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        scrollTopButton.classList.add("show");
+    } else {
+        scrollTopButton.classList.remove("show");
+    }
+});
+
+scrollTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
